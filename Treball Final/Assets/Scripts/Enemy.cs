e46f03bc799fd37c5dd4a3080fbea327dda58d08
bfontbute <bfontbute@gmail.com>
2022-05-02 11:56:05 +0200

    enemy movement

---
+++ Treball Final/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform[] waypoints;
+    int current;
+    public float speed;
+
+
+    void Start()
+    {
+        current = 0;
+    }
+
+
+    void Update()
+    {
+        if (transform.position != waypoints[current].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[current].position, speed * Time.deltaTime);
+        }
+        else
+            current = (current + 1) % waypoints.Length;
+    }
+
+}
